6122e1985f54fd61ff5bcb9977a5f685ceccd5d5
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-09-24 22:44:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,25 +27,11 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  season_keys = 
-  holiday_hash.each do |key|
-    key.to_s.capitalize!
-  end
-  
-  holiday_keys = 
-  holiday_hash.each do |key, value|
-    values = value.to_s.split("_")
-      values.each do |x|
-        x.capitalize!
-      end
-    end
-  
-  supplies =
   holiday_hash.each do |season, holiday|
-    holiday.each do |supply|
-      supply.join(" ")
-    end
-  end
+    
+    holiday.each do |
+  
+ 
   
   
       
