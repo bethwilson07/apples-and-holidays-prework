78a2104b8a68970a8fcb225be666a2dabf6d5b7a
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-09-24 23:32:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,9 +37,9 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holidays|
-    holidays.collect do |supplies|
+    holidays.collect do |holiday, supplies|
       if supplies == "BBQ"
-        puts holidays
+        puts holiday
       end
     end
   end
